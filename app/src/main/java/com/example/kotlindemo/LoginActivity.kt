dc0bf3899d10d53a8465db7199142ed8d994b33f
hongming.low <hongming.low@atoz-software.tech>
2024-07-13 16:22:45 +0800

    toogle the password visibility

--- app/src/main/java/com/example/kotlindemo/LoginActivity.kt
+++ app/src/main/java/com/example/kotlindemo/LoginActivity.kt
@@ -2,6 +2,10 @@ package com.example.kotlindemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModelProvider
@@ -12,15 +16,21 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var viewModel: LoginViewModel
+    private lateinit var passwordEditText: EditText
+    private var isPasswordVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        passwordEditText = findViewById(R.id.password_edit_text)
 
         // Initialize ViewModel
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
 
+
+        // Set default value to 8 asterisks
+//        passwordEditText.setText("********")
 
         binding.loginButton.setOnClickListener {
             // Implement your authentication logic here
